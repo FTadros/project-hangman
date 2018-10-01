a96e92e008d0cb985e3342af91ad70b51c90bb1b
ray1i <43012140+ray1i@users.noreply.github.com>
2018-10-01 13:12:25 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,2 +1,19 @@
-# hangman.py 
-    
+# hangman.py
+def display_platform(wrong_guesses):
+  if len(wrong_guesses)==0:
+    print(' _____'+'\n'+' |'+'\n'+' |'+'\n'+' |'+'\n'+'_|__')
+  elif len(wrong_guesses)==1:
+    print(' _____'+'\n'+' |  Ó'+'\n'+' |'+'\n'+' |'+'\n'+'_|__')
+  elif len(wrong_guesses)==2:
+    print(' _____'+'\n'+' |  Ó'+'\n'+' |  |'+'\n'+' |'+'\n'+'_|__')
+  elif len(wrong_guesses)==3:
+    print(' _____'+'\n'+' |  Ó'+'\n'+' | /|'+'\n'+' |'+'\n'+'_|__')
+  elif len(wrong_guesses)==4:
+    print(' _____'+'\n'+' |  Ó'+'\n'+' | /|\ '+'\n'+' |'+'\n'+'_|__')
+  elif len(wrong_guesses)==5:
+    print(' _____'+'\n'+' |  Ó'+'\n'+' | /|\ '+'\n'+' | /'+'\n'+'_|__')
+  else:
+    print(' _____'+'\n'+' |  Ó'+'\n'+' | /|\ '+'\n'+' | / \ '+'\n'+'_|__')
+#assert display_platform('s')==print(' _____'+'\n'+' |  Ó'+'\n'+' |'+'\n'+' |'+'\n'+'_|__')
+#assert display_platform('uhe') ==print (' _____'+'\n'+' |  Ó'+'\n'+' | /|'+'\n'+' |'+'\n'+'_|__')
+#assert display_platform('ijufxb) ==print (' _____'+'\n'+' |  Ó'+'\n'+' | /|\ '+'\n'+' | / \ '+'\n'+'_|__')
